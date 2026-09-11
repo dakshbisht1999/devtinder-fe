adafc75005986851d2b252d242c0502d30f0161c
Dishant Bisht <53486763+dakshbisht1999@users.noreply.github.com>
2026-09-11 06:12:37 +0530

    set baseURL dynamically using VITE_ in .env file and added .env file in .gitignore for security

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+// Vite will automatically inject the correct URL here
+const baseURL = import.meta.env.VITE_BASE_URL;
+
 // Ek global 'instance' banaya hai, jisme saari default settings hai
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:7777/api/v1", // Aapka backend URL
+    baseURL: baseURL, // Aapka backend URL
     withCredentials: true // automatically sends everytime, also I send withCredentials true so that browser security engine completely allows Set-Cookie headers from cross-origin APIs
 });
 
