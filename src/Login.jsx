a3f4d19829c2d893d9470d318595649b8c31e2c9
Dishant Bisht <53486763+dakshbisht1999@users.noreply.github.com>
2026-09-11 05:35:22 +0530

    added password validators

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,33 +1,48 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import axiosInstance from "./utils/axios";
 
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [emailError, setEmailError] = useState("");
+    const [emailId, setEmailId] = useState("aliabhatt1@gmail.com");
+    const [emailIdError, setEmailIdError] = useState("");
+    const [password, setPassword] = useState("Abcd@1234");
+    const [passwordError, setPasswordError] = useState("");
 
     const validateEmail = (value) => {
         const error = !value.trim()
             ? "Required"
             : !emailPattern.test(value)
-                ? "Enter a valid email password"
+                ? "Enter a valid email address."
                 : "";
 
-        setEmailError(error);
+        setEmailIdError(error);
         return !error;
     };
 
-    const handleSubmit = (event) => {
+    const validatePassword = (value) => {
+        const error = !value.trim() ? "Required" : "";
+
+        setPasswordError(error);
+        return !error;
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        validateEmail(email);
+
+        const isEmailValid = validateEmail(emailId);
+        const isPasswordValid = validatePassword(password);
+
+        if (!isEmailValid || !isPasswordValid) return;
+
     };
 
     return (
         <>
             {/* <h1>Login Page</h1> */}
             <div className="flex justify-center items-center my-20">
-                <div className="card bg-base-200 w-96 shadow-sm ">
+                <div className="card bg-base-300 w-96 shadow-sm ">
                     {/* <figure>
                         <img
                         src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
@@ -38,7 +53,7 @@ const Login = () => {
                             LOGIN @DevTinder
                         </h2>
 
-                        <form className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4"
+                        <form className="fieldset w-xs p-4"
                             noValidate onSubmit={handleSubmit}>
                             <fieldset className="fieldset">
                                 <label className="label">Email</label>
@@ -46,26 +61,38 @@ const Login = () => {
                                     type="email"
                                     className="input validator"
                                     placeholder="Email"
-                                    value={email}
+                                    value={emailId}
                                     onChange={(event) => {
                                         const value = event.target.value;
-                                        setEmail(value);
-                                        if (emailError) validateEmail(value);
+                                        setEmailId(value);
+                                        if (emailIdError) validateEmail(value);
                                     }}
                                     onBlur={(event) => validateEmail(event.target.value)}
-                                    aria-invalid={Boolean(emailError)}
+                                    aria-invalid={Boolean(emailIdError)}
                                 />
-                                {emailError && <p className="validator-hint">{emailError}</p>}
+                                {emailIdError && <p className="validator-hint">{emailIdError}</p>}
                             </fieldset>
 
                             <label className="fieldset">
                                 <span className="label">Password</span>
-                                <input type="password" className="input validator" placeholder="Password" required />
-                                <span className="validator-hint hidden">Required</span>
+                                <input 
+                                    type="password" 
+                                    className="input validator" 
+                                    placeholder="Password" 
+                                    value={password}
+                                    onChange={(event) => {
+                                        const value = event.target.value;
+                                        setPassword(value);
+                                        if (passwordError) validatePassword(value);
+                                    }}
+                                    onBlur={(event) => validatePassword(event.target.value)}
+                                    aria-invalid={Boolean(passwordError)}
+                                />
+                                {passwordError && <span className="validator-hint">{passwordError}</span>}
                             </label>
 
-                            <button className="btn btn-neutral mt-4" type="submit">Login</button>
-                            <Link className="btn btn-ghost mt-1" to="/resetPassword">Reset Password</Link>
+                            <button className="btn btn-primary mt-4" type="submit">Login</button>
+                            <Link className="btn btn-soft btn-warning mt-1" to="/resetPassword">Reset Password</Link>
                         </form>
                     </div>
                 </div>
