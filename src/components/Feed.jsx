83be59e1bf974036b97e5bade2d39bcf701d7531
Dishant Bisht <53486763+dakshbisht1999@users.noreply.github.com>
2026-09-14 20:30:01 +0530

    created feed page, integrated feed api along with send request interested/ignore apis [using redux store]

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,39 @@
-const Feed = () => {
-    return (
-        <>
-            Feed Page
-        </>
-    )
-}
+import { useDispatch, useSelector } from "react-redux";
+import axiosInstance from "../utils/axios";
+import { addFeed } from "../utils/feedSlice";
+import { handleApiError } from "../utils/errorHandler";
+import { useCallback, useEffect } from "react";
+import UserCard from "./UserCard";
 
-export default Feed
+const Feed = () => {
+
+
+    const feed = useSelector((store) => store.feed);
+    const dispatch = useDispatch();
+
+    const getFeed = useCallback(async () => {
+        try {
+            const res = await axiosInstance.get("/user/feed", {
+                params: { limit: 10, page: 1 },
+            });
+
+            if (res.data.success) {
+                dispatch(addFeed(res.data.data));
+            }
+        } catch (err) {
+            handleApiError(err);
+        }
+    }, [dispatch]);
+
+    useEffect(() => {
+        getFeed();
+    }, [getFeed]);
+
+    if (!feed.length) {
+        return <p className="mt-10 text-center">No more profiles to show.</p>;
+    }
+
+    return <UserCard users={feed} />;
+};
+
+export default Feed;
