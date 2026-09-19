4fc424f984ad476a6bbb7026699322064aec705c
Dishant Bisht <53486763+dakshbisht1999@users.noreply.github.com>
2026-09-19 20:39:42 +0530

    changed baseURL logic for prod testing

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import { handleApiError, isLoginRequest } from './errorHandler';
+import { BASE_URL } from './constants';
 
 // Vite will automatically inject the correct URL here
-const baseURL = import.meta.env.VITE_BASE_URL || "/api/v1";
+// const baseURL = import.meta.env.VITE_BASE_URL || "/api/v1";
 
 // Ek global 'instance' banaya hai, jisme saari default settings hai
 const axiosInstance = axios.create({
-    baseURL: baseURL, // Aapka backend URL
+    baseURL: BASE_URL+"/api/v1", // Aapka backend URL
     withCredentials: true // automatically sends everytime, also I send withCredentials true so that browser security engine completely allows Set-Cookie headers from cross-origin APIs
 });
 
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:7777" : "http://13.54.230.74:7777";
