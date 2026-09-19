9e05ea0f5e729f2faefeae3b5afd962d1ac01577
Dishant Bisht <53486763+dakshbisht1999@users.noreply.github.com>
2026-09-19 21:27:03 +0530

    corrected the baseURL logic [after adding the proxy in the prod-fe nginx config]

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import { handleApiError, isLoginRequest } from './errorHandler';
-import { BASE_URL } from './constants';
 
 // Vite will automatically inject the correct URL here
-// const baseURL = import.meta.env.VITE_BASE_URL || "/api/v1";
+const baseURL = import.meta.env.VITE_BASE_URL || "/api/v1";
 
 // Ek global 'instance' banaya hai, jisme saari default settings hai
 const axiosInstance = axios.create({
-    baseURL: BASE_URL+"/api/v1", // Aapka backend URL
+    baseURL: baseURL, // Aapka backend URL
     withCredentials: true // automatically sends everytime, also I send withCredentials true so that browser security engine completely allows Set-Cookie headers from cross-origin APIs
 });
 
--- src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:7777" : "http://13.54.230.74:7777";
